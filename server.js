c7b3b04d339e8ad5fe532f30d89ce29f14b68c48
Linear.DouglasFiuza <douglas.junior@linearsistemas.com.br>
2020-05-14 14:47:22 -0300

    git

---
+++ server.js
@@ -0,0 +1,35 @@
+require ('dotenv').config();
+
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+const moogoose = require('mongoose')
+
+moogoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+  console.log('Conectei à base de dados.');
+  app.emit('pronto');
+}).
+catch(e => console.log(e));
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+// Nossos próprios middlewares
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.on('pronto', () =>{
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+  });  
+});
+
